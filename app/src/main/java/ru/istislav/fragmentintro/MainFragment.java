89d00242412de43685e62d006a5172152a0dc1b9
CyberUnit <cyberunit@gmail.com>
2020-08-04 17:57:54 +0300

    insert new fragment

---
+++ app/src/main/java/ru/istislav/fragmentintro/MainFragment.java
@@ -0,0 +1,26 @@
+package ru.istislav.fragmentintro;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MainFragment extends androidx.fragment.app.Fragment {
+
+    public MainFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
+        //return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
